a6307ca30a34c821812127c65966b7d4915c8ac5
89045 <msari5734@gmail.com>
2022-03-17 21:58:00 +0300

    All Project Files Uploaded

--- src/test/java/Day1/Git1.java
+++ src/test/java/Day1/Git1.java
@@ -1,2 +1,5 @@
-package Day1;public class Git1 {
+package Day1;
+
+public class Git1 {
+    //git1 eklendi ve değiştirildi--MODİFİED
 }
--- src/test/java/Day1/Git3.java
+++ src/test/java/Day1/Git3.java
@@ -1,2 +1,6 @@
-package Day1;public class Git3 {
+package Day1;
+
+public class Git3 {
+    //Git oluştu
+    //Modifiye
 }
