15db27cd6ba9019c5b28b76432e0a1517f5fd8e9
89045 <msari5734@gmail.com>
2022-03-17 22:05:30 +0300

    Git1 and Git3 files updates

--- src/test/java/Day1/Git1.java
+++ src/test/java/Day1/Git1.java
@@ -2,4 +2,6 @@ package Day1;
 
 public class Git1 {
     //git1 eklendi ve değiştirildi--MODİFİED
+
+    // commit sonrası git1 değişti
 }
--- src/test/java/Day1/Git3.java
+++ src/test/java/Day1/Git3.java
@@ -3,4 +3,5 @@ package Day1;
 public class Git3 {
     //Git oluştu
     //Modifiye
+    // commit sonrası git1 değişti
 }
